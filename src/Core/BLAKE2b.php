be7539cf5b8dbd59e5b51b66319c9e5afbb0d7a3
Paragon Initiative Enterprises, LLC <security@paragonie.com>
2017-01-03 01:00:15 -0500

    Fix PHP < 5.4

--- src/Core/BLAKE2b.php
+++ src/Core/BLAKE2b.php
@@ -13,22 +13,25 @@ abstract class ParagonIE_Sodium_Core_BLAKE2b extends ParagonIE_Sodium_Core_Util
     protected static $iv;
 
     /**
-     * @var int[][]
+     * @return int[][]
      */
-    protected static $sigma = array(
-        array(  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15),
-        array( 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3),
-        array( 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4),
-        array(  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8),
-        array(  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13),
-        array(  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9),
-        array( 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11),
-        array( 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10),
-        array(  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5),
-        array( 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13 , 0),
-        array(  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15),
-        array( 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3)
-    );
+    protected static function sigma()
+    {
+        return array(
+            array(  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15),
+            array( 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3),
+            array( 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4),
+            array(  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8),
+            array(  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13),
+            array(  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9),
+            array( 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11),
+            array( 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10),
+            array(  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5),
+            array( 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13 , 0),
+            array(  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15),
+            array( 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3)
+        );
+    }
 
     const BLOCKBYTES = 128;
     const OUTBYTES   = 64;
@@ -257,12 +260,14 @@ abstract class ParagonIE_Sodium_Core_BLAKE2b extends ParagonIE_Sodium_Core_Util
         $v[14] = self::xor64($ctx[2][0], self::$iv[6]);
         $v[15] = self::xor64($ctx[2][1], self::$iv[7]);
 
-        $G = function ($r, $i, $a, $b, $c, $d) use ($v, $m) {
-            $v[$a] = self::add364($v[$a], $v[$b], $m[self::$sigma[$r][2*$i]]);
+        $sigma = self::sigma();
+
+        $G = function ($r, $i, $a, $b, $c, $d) use ($v, $m, $sigma) {
+            $v[$a] = self::add364($v[$a], $v[$b], $m[$sigma[$r][2*$i]]);
             $v[$d] = self::rotr64(self::xor64($v[$d], $v[$a]), 32);
             $v[$c] = self::add64($v[$c], $v[$d]);
             $v[$b] = self::rotr64(self::xor64($v[$b], $v[$c]), 24);
-            $v[$a] = self::add364($v[$a], $v[$b], $m[self::$sigma[$r][2*$i+1]]);
+            $v[$a] = self::add364($v[$a], $v[$b], $m[$sigma[$r][2*$i+1]]);
             $v[$d] = self::rotr64(self::xor64($v[$d], $v[$a]), 16);
             $v[$c] = self::add64($v[$c], $v[$d]);
             $v[$b] = self::rotr64(self::xor64($v[$b], $v[$c]), 63);
