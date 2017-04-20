fc00c1da6f1c23848eff660fbb20b4469fe2d4fe
Paragon Initiative Enterprises <security@paragonie.com>
2017-04-20 15:24:59 -0400

    Fix namespace unit test for Travis CI.

--- tests/compat/NamespacedTest.php
+++ tests/compat/NamespacedTest.php
@@ -26,7 +26,12 @@ class NamespacedTest extends PHPUnit_Framework_TestCase
 
         $this->assertSame(
             ParagonIE_Sodium_Core_Util::substr(
-                bin2hex($boxfunc($message, $nonce, $key)),
+                bin2hex(
+                    call_user_func_array(
+                        array('\\ParagonIE\\Sodium\\Compat', 'crypto_secretbox'),
+                        array($message, $nonce, $key)
+                    )
+                ),
                 0, 32
             ),
             ParagonIE_Sodium_Core_Util::substr(
@@ -45,15 +50,23 @@ class NamespacedTest extends PHPUnit_Framework_TestCase
         );
         $this->assertSame(
             $message,
-            $openfunc(
-                ParagonIE_Sodium_Crypto::secretbox($message, $nonce, $key),
-                $nonce,
-                $key
+            call_user_func_array(
+                array('\\ParagonIE\\Sodium\\Compat', 'crypto_secretbox_open'),
+                array(
+                    ParagonIE_Sodium_Crypto::secretbox($message, $nonce, $key),
+                    $nonce,
+                    $key
+                )
             )
         );
         $message = str_repeat('a', 97);
         $this->assertSame(
-            bin2hex($boxfunc($message, $nonce, $key)),
+            bin2hex(
+                call_user_func_array(
+                    array('\\ParagonIE\\Sodium\\Compat', 'crypto_secretbox'),
+                    array($message, $nonce, $key)
+                )
+            ),
             bin2hex(ParagonIE_Sodium_Crypto::secretbox($message, $nonce, $key)),
             'secretbox - long messages (multiple of 16)'
         );
@@ -63,7 +76,12 @@ class NamespacedTest extends PHPUnit_Framework_TestCase
         $message = str_repeat($message, 16);
 
         $this->assertSame(
-            bin2hex($boxfunc($message, $nonce, $key)),
+            bin2hex(
+                call_user_func_array(
+                    array('\\ParagonIE\\Sodium\\Compat', 'crypto_secretbox'),
+                    array($message, $nonce, $key)
+                )
+            ),
             bin2hex(ParagonIE_Sodium_Crypto::secretbox($message, $nonce, $key)),
             'secretbox - long messages (multiple of 16)'
         );
@@ -71,7 +89,12 @@ class NamespacedTest extends PHPUnit_Framework_TestCase
         $message .= 'a';
 
         $this->assertSame(
-            bin2hex($boxfunc($message, $nonce, $key)),
+            bin2hex(
+                call_user_func_array(
+                    array('\\ParagonIE\\Sodium\\Compat', 'crypto_secretbox'),
+                    array($message, $nonce, $key)
+                )
+            ),
             bin2hex(ParagonIE_Sodium_Crypto::secretbox($message, $nonce, $key)),
             'secretbox - long messages (NOT a multiple of 16)'
         );
@@ -79,7 +102,12 @@ class NamespacedTest extends PHPUnit_Framework_TestCase
         $message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
 
         $this->assertSame(
-            bin2hex($boxfunc($message, $nonce, $key)),
+            bin2hex(
+                call_user_func_array(
+                    array('\\ParagonIE\\Sodium\\Compat', 'crypto_secretbox'),
+                    array($message, $nonce, $key)
+                )
+            ),
             bin2hex(ParagonIE_Sodium_Crypto::secretbox($message, $nonce, $key)),
             'secretbox - medium messages'
         );
