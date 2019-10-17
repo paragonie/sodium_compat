58fbadbd6ccd1dab1e16cef5f6a40b8d16f781f4
Paragon Initiative Enterprises <security@paragonie.com>
2019-10-17 16:58:11 -0400

    Exit immediately upon failure

--- tests/travis-php52.sh
+++ tests/travis-php52.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
 php php52-phpunit.phar
+EXITCOMMAND=$?
+if [[ "$EXITCOMMAND" -ne 0 ]]; then exit $EXITCOMMAND; fi
+
 php php52-phpunit.phar --bootstrap autoload-fast.php
+EXITCOMMAND=$?
+if [[ "$EXITCOMMAND" -ne 0 ]]; then exit $EXITCOMMAND; fi
+
 if [[ $CHECK_MBSTRING -eq 1 ]]; then
   php -dmbstring.func_overload=7 php52-phpunit.phar
+  EXITCOMMAND=$?
+  if [[ "$EXITCOMMAND" -ne 0 ]]; then exit $EXITCOMMAND; fi
   php -dmbstring.func_overload=7 php52-phpunit.phar --bootstrap autoload-fast.php
+  EXITCOMMAND=$?
+  if [[ "$EXITCOMMAND" -ne 0 ]]; then exit $EXITCOMMAND; fi
 fi
