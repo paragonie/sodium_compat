83161d6bbfecf293ca799d67bf6add76656e9e5d
Paragon Initiative Enterprises <security@paragonie.com>
2019-10-18 13:02:07 -0400

    Print out return value when bash script fails

--- tests/travis-php52.sh
+++ tests/travis-php52.sh
@@ -2,17 +2,29 @@
 
 php php52-phpunit.phar
 EXITCOMMAND=$?
-if [[ "$EXITCOMMAND" -ne 0 ]]; then exit $EXITCOMMAND; fi
+if [[ "$EXITCOMMAND" -ne 0 ]]; then
+  echo "PHPunit (first run) exited with return value $EXITCOMMAND."
+  exit $EXITCOMMAND
+fi
 
 php php52-phpunit.phar --bootstrap autoload-fast.php
 EXITCOMMAND=$?
-if [[ "$EXITCOMMAND" -ne 0 ]]; then exit $EXITCOMMAND; fi
+if [[ "$EXITCOMMAND" -ne 0 ]]; then
+  echo "PHPunit (second run) exited with return value $EXITCOMMAND."
+  exit $EXITCOMMAND
+fi
 
 if [[ $CHECK_MBSTRING -eq 1 ]]; then
   php -dmbstring.func_overload=7 php52-phpunit.phar
   EXITCOMMAND=$?
-  if [[ "$EXITCOMMAND" -ne 0 ]]; then exit $EXITCOMMAND; fi
+  if [[ "$EXITCOMMAND" -ne 0 ]]; then
+    echo "PHPunit (third run) exited with return value $EXITCOMMAND."
+    exit $EXITCOMMAND
+  fi
   php -dmbstring.func_overload=7 php52-phpunit.phar --bootstrap autoload-fast.php
   EXITCOMMAND=$?
-  if [[ "$EXITCOMMAND" -ne 0 ]]; then exit $EXITCOMMAND; fi
+  if [[ "$EXITCOMMAND" -ne 0 ]]; then
+    echo "PHPunit (fourth run) exited with return value $EXITCOMMAND."
+    exit $EXITCOMMAND
+  fi
 fi
