f3bfe50ca79f7f549cf9a4436a4dec468a64cec3
Paragon Initiative Enterprises <security@paragonie.com>
2022-03-30 04:02:40 -0400

    Add sodium_crypto_stream_xchacha20_xor_ic()
    See https://github.com/php/php-src/pull/8276 for upstream

--- autoload.php
+++ autoload.php
@@ -69,4 +69,5 @@ if (PHP_VERSION_ID < 70200 || !extension_loaded('sodium')) {
     // Older versions of {PHP, ext/sodium} will not define these
     require_once(dirname(__FILE__) . '/lib/php72compat.php');
 }
+require_once(dirname(__FILE__) . '/lib/stream-xchacha20.php');
 require_once(dirname(__FILE__) . '/lib/ristretto255.php');
