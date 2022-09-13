716deda431d4f3cbae205f145e27be2dcaa90d4f
Paragon Initiative Enterprises <security@paragonie.com>
2022-09-13 16:41:37 -0400

    For consistency, don't autoload in check

--- src/Core/Ed25519.php
+++ src/Core/Ed25519.php
@@ -3,7 +3,7 @@
 if (class_exists('ParagonIE_Sodium_Core_Ed25519', false)) {
     return;
 }
-if (!class_exists('ParagonIE_Sodium_Core_Curve25519')) {
+if (!class_exists('ParagonIE_Sodium_Core_Curve25519', false)) {
     require_once dirname(__FILE__) . '/Curve25519.php';
 }
 
