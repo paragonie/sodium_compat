906e0b925895d3a5941eda25f371fbafb3cbc22f
Paragon Initiative Enterprises <security@paragonie.com>
2022-09-13 16:54:27 -0400

    Fix build scripts

--- build-phar.sh
+++ build-phar.sh
@@ -4,5 +4,5 @@ curl -LSs https://box-project.github.io/box2/installer.php | php
 mkdir ~/box
 mv box.phar ~/box/box
 PATH=$PATH:~/box/ make -C dist/ build-phar
-# PATH=$PATH:~/box/ make -C dist/ sign-phar
+PATH=$PATH:~/box/ make -C dist/ sign-phar
 
