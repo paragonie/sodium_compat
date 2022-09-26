a63ccaca72cc772070af7cf232f151cdb8f08e63
Paragon Initiative Enterprises <security@paragonie.com>
2022-09-25 23:28:58 -0400

    Fix: int/char disagreement
    Why did Psalm not catch this?

--- src/Core/Util.php
+++ src/Core/Util.php
@@ -351,7 +351,7 @@ abstract class ParagonIE_Sodium_Core_Util
             $c_alpha = ($c & ~32) - 55;
             $c_alpha0 = (($c_alpha - 10) ^ ($c_alpha - 16)) >> 8;
             if (($c_num0 | $c_alpha0) === 0) {
-                if ($ignore && $state === 0 && strpos($ignore, $c) !== false) {
+                if ($ignore && $state === 0 && strpos($ignore, self::intToChr($c)) !== false) {
                     ++$hex_pos;
                     continue;
                 }
