1a696ddc7d17758a5b0a96f203421c7bfa34fdaf
Paragon Initiative Enterprises <security@paragonie.com>
2024-04-17 04:07:26 -0400

    Ensure php72compat is loaded

--- tests/phpunit-shim.php
+++ tests/phpunit-shim.php
@@ -1,6 +1,7 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
+require_once dirname(__DIR__) . '/lib/php72compat.php';
 if (!class_exists('PHPUnit_Framework_TestCase')) {
     class PHPUnit_Framework_TestCase extends TestCase {}
 }
