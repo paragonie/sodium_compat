249d00b5c6eaf3d64b282e70c4bd2a15c2280b01
Paragon Initiative Enterprises <security@paragonie.com>
2024-04-17 04:18:35 -0400

    Move PHP 8.4-compat to its own file

--- tests/phpunit-shim.php
+++ tests/phpunit-shim.php
@@ -1,7 +1,6 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-require_once dirname(__DIR__) . '/lib/php72compat.php';
 if (!class_exists('PHPUnit_Framework_TestCase')) {
     class PHPUnit_Framework_TestCase extends TestCase {}
 }
