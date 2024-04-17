f6c4161da2aa56015d39ea5705fb3c3b80fbb363
Paragon Initiative Enterprises <security@paragonie.com>
2024-04-17 03:50:14 -0400

    Add compatibility tests for PHP 8.4+

---
+++ tests/compat/PHP84Test.php
@@ -0,0 +1,45 @@
+<?php
+
+class PHP84Test extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @before
+     */
+    public function before()
+    {
+        if (PHP_VERSION_ID < 80400) {
+            $this->markTestSkipped('PHP < 8.4.0; skipping PHP 8.4 compatibility test suite.');
+        }
+        ParagonIE_Sodium_Compat::$disableFallbackForUnitTests = true;
+    }
+
+    public function testAegis128l()
+    {
+        $msg = ParagonIE_Sodium_Compat::randombytes_buf(ParagonIE_Sodium_Compat::randombytes_uniform(999) + 1);
+        $nonce = ParagonIE_Sodium_Compat::randombytes_buf(ParagonIE_Sodium_Compat::CRYPTO_AEAD_AEGIS128L_NPUBBYTES);
+        $ad = ParagonIE_Sodium_Compat::randombytes_buf(ParagonIE_Sodium_Compat::randombytes_uniform(999) + 1);
+        $key = ParagonIE_Sodium_Compat::crypto_aead_aegis128l_keygen();
+        $ciphertext = ParagonIE_Sodium_Compat::crypto_aead_aegis128l_encrypt($msg, $ad, $nonce, $key);
+        $c2 = sodium_crypto_aead_aegis128l_encrypt($msg, $ad, $nonce, $key);
+        $this->assertSame($ciphertext, $c2);
+        $msg2 = ParagonIE_Sodium_Compat::crypto_aead_aegis128l_decrypt($ciphertext, $ad, $nonce, $key);
+        $msg2b = sodium_crypto_aead_aegis128l_decrypt($c2, $ad, $nonce, $key);
+        $this->assertSame($msg, $msg2);
+        $this->assertSame($msg, $msg2b);
+    }
+
+    public function testAegis256()
+    {
+        $msg = ParagonIE_Sodium_Compat::randombytes_buf(ParagonIE_Sodium_Compat::randombytes_uniform(999) + 1);
+        $nonce = ParagonIE_Sodium_Compat::randombytes_buf(ParagonIE_Sodium_Compat::CRYPTO_AEAD_AEGIS256_NPUBBYTES);
+        $ad = ParagonIE_Sodium_Compat::randombytes_buf(ParagonIE_Sodium_Compat::randombytes_uniform(999) + 1);
+        $key = ParagonIE_Sodium_Compat::crypto_aead_aegis256_keygen();
+        $ciphertext = ParagonIE_Sodium_Compat::crypto_aead_aegis256_encrypt($msg, $ad, $nonce, $key);
+        $c2 = sodium_crypto_aead_aegis256_encrypt($msg, $ad, $nonce, $key);
+        $this->assertSame($ciphertext, $c2);
+        $msg2 = ParagonIE_Sodium_Compat::crypto_aead_aegis256_decrypt($ciphertext, $ad, $nonce, $key);
+        $msg2b = sodium_crypto_aead_aegis256_decrypt($c2, $ad, $nonce, $key);
+        $this->assertSame($msg, $msg2);
+        $this->assertSame($msg, $msg2b);
+    }
+}
